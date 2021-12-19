5014acf4ebccf0396c49aa5692c8b7454d42b4fe
saketvardhan <76768478+saketvardhan@users.noreply.github.com>
2021-12-19 19:45:00 +0530

    paac_portfolio page

---
+++ app.js
@@ -0,0 +1,25 @@
+const menu_button = document.querySelector('.header .nav-bar .nav-list .menu_button');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+menu_button.addEventListener('click', () => {
+	menu_button.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		menu_button.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
